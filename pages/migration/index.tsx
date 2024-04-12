c821ff832bdc56c7b9f48137d0d04630aa23d376
Rolwes <m235370@academy.usna.edu>
2024-04-11 20:00:26 -0500

    updating_file_system

--- pages/migration/index.tsx
+++ pages/migration/index.tsx
@@ -2,10 +2,10 @@ import { ConnectButton } from '@rainbow-me/rainbowkit';
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
-import { Footer } from '../components/Footer';
-import { Header } from '../components/Header';
-import { TopSection } from '../components/TopSection';
-import { Migration } from '../components/Migration';
+import { Footer } from '../../components/Footer';
+import { Header } from '../../components/Header';
+import { TopSection } from '../../components/TopSection';
+import { Migration } from '../../components/Migration';
 
 const Home: NextPage = () => {
   return (
