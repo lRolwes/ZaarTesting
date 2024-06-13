fcfc82b98911e796e86f560026823a1e3da3f7d4
Louis Rolwes <lbRolwes@gmail.com>
2024-06-12 19:18:02 -0500

    updating cors policy

--- pages/api/addItem.ts
+++ pages/api/addItem.ts
@@ -3,9 +3,24 @@ import { PrismaClient } from '@prisma/client';
 import prisma from '../../lib/prisma';
 import { create } from 'domain';
 import initializeCors from 'nextjs-cors';
-
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    await initializeCors(req, res); // Initialize CORS
+const allowCors = (fn: { (req: NextApiRequest, res: NextApiResponse): Promise<void>; (arg0: any, arg1: any): any; }) => async (req: { method: string; }, res: { setHeader: (arg0: string, arg1: string | boolean) => void; status: (arg0: number) => { (): any; new(): any; end: { (): void; new(): any; }; }; }) => {
+    res.setHeader('Access-Control-Allow-Credentials', true)
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    // another common pattern
+    // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+    res.setHeader(
+      'Access-Control-Allow-Headers',
+      'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+    )
+    if (req.method === 'OPTIONS') {
+      res.status(200).end()
+      return
+    }
+    return await fn(req, res)
+  }
+async function handler(req: NextApiRequest, res: NextApiResponse) {
+    //await initializeCors(req, res); // Initialize CORS
 
     try {
     let address = (req.query.address as string)?.split(",") || []; // Assuming the address is passed as a query parameter
@@ -44,3 +59,4 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   }
 }
 
+module.exports = allowCors(handler);
--- pages/api/getRewards.ts
+++ pages/api/getRewards.ts
@@ -5,9 +5,25 @@ import { PrismaClient } from '@prisma/client';
 import prisma from '../../lib/prisma';
 import initializeCors from 'nextjs-cors';
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  await initializeCors(req, res); // Initialize CORS
-
+const allowCors = (fn: { (req: NextApiRequest, res: NextApiResponse): Promise<void>; (arg0: any, arg1: any): any; }) => async (req: { method: string; }, res: { setHeader: (arg0: string, arg1: string | boolean) => void; status: (arg0: number) => { (): any; new(): any; end: { (): void; new(): any; }; }; }) => {
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
+  if (req.method === 'OPTIONS') {
+    res.status(200).end()
+    return
+  }
+  return await fn(req, res)
+}
+async function handler(req: NextApiRequest, res: NextApiResponse) {
+  //await initializeCors(req, res); // Initialize CORS
+  
   try {
     // Query your Prisma database based on the user's address
     const ownerAddress = req.query.ownerAddress?.toString(); // Assuming the address is passed as a query parameter
@@ -23,3 +39,5 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     res.status(500).json({ error: 'Internal server error' });
   }
 }
+module.exports = allowCors(handler);
+
